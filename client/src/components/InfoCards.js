dea1611a46e2c13b48a9c20ff513a98716960acc
Kalvine <kkalvine4@gmail.com>
2024-10-15 14:03:43 +0300

    Adds new cards to InfoCards

--- client/src/components/InfoCards.js
+++ client/src/components/InfoCards.js
@@ -3,7 +3,21 @@ import { Link } from "react-router-dom";
 
 function InfoCards() {
     return (
+        <>
         <div className="infoCards">
+            <h1>What can you do with TibaCare?</h1>
+            <div className="getInfo">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getInfo">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getInfo">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
             <div className="getInfo">
                 <h3>Ask for assistance</h3>
                 <Link to='' >Learn More</Link>
@@ -17,7 +31,34 @@ function InfoCards() {
                 <Link to='' >Learn More</Link>
             </div>
         </div>
-
+        <div className="aboutCards">
+            <h1>Why Us?</h1>
+            <div className="getAbout">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getAbout">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getAbout">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getAbout">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getAbout">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+            <div className="getAbout">
+                <h3>Ask for assistance</h3>
+                <Link to='' >Learn More</Link>
+            </div>
+        </div>
+        </> 
     )
 }
 
