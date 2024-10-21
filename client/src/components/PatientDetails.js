9707b889f3b4d487dc7db5254b8d1985dfb34ed8
dennis <kipkiruidennis25@gmail.com>
2024-10-21 12:17:05 +0300

    patientdetails update

--- client/src/components/PatientDetails.js
+++ client/src/components/PatientDetails.js
@@ -23,7 +23,7 @@ const PatientDetails = () => {
           setError(error.message);
         });
     }
-  }, [user.id]);
+  }, [user]);
 
   if (error) {
     return <p>Error: {error}</p>;
@@ -36,10 +36,12 @@ const PatientDetails = () => {
 
   return (
     <div className="patient-details">
-      <h2>Patient Details</h2>
+      <h2><strong>Patient Details</strong></h2>
       <div>
-        <p><strong>Name:</strong> {patient.first_name}</p>
+        <p><strong>Name:</strong> {patient.first_name} {patient.last_name}</p>
+        <p><strong>Gender:</strong>{patient.gender}</p>
         <p><strong>Age:</strong> {patient.age}</p>
+        <p><strong>Phone_number:</strong> {patient.phone_number}</p>
         <p><strong>Medical Records:</strong> {patient.medical_records?.join(', ') || 'No medical records found'}</p>
       </div>
     </div>
